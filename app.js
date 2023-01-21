5f09540ff8f63377e2e372f97f26b3d0fe556b54
tonmoy <tanverislamtonmoy2929@gmail.com>
2023-01-21 23:34:01 +0600

    attribute binding done

--- app.js
+++ app.js
@@ -6,35 +6,36 @@ const app = Vue.createApp({
 
   data() {
     return {
+      url: 'https://vuejs.org/guide/quick-start.html#using-vue-from-cdn',
       title: 'hello beautiful world',
       age: 4,
       showContent: true,
       x: 0,
       y: 0,
       books: [
-        {title : 'Dopamine detox', author: 'Thibaut Meurisse'},
-        {title : 'Avatar:The way of water', author: 'James Cameron'},
-        {title : 'Rich dad, Poor dad', author: 'Robert T'},
-      ]
+        { title: 'Dopamine detox', author: 'Thibaut Meurisse', img: 'assets/1.jpg' },
+        { title: 'Avatar:The way of water', author: 'James Cameron', img: 'assets/2.jpg' },
+        { title: 'Rich dad, Poor dad', author: 'Robert T', img: 'assets/3.jpg' },
+      ],
     };
   },
   methods: {
     changeTitle() {
-      this.title = 'Is it working properly 😌'
+      this.title = 'Is it working properly 😌';
     },
-    showContentToggle(){
-        this.showContent = !this.showContent
+    showContentToggle() {
+      this.showContent = !this.showContent;
     },
-    handleEvents(e, data){
-        console.log(e, e.type)
-        if(data){
-            console.log(data);
-        }
+    handleEvents(e, data) {
+      console.log(e, e.type);
+      if (data) {
+        console.log(data);
+      }
     },
-    handleMousemove(e){
-        this.x = e.offsetX;
-        this.y = e.offsetY;
-    }
+    handleMousemove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+    },
   },
 });
 
